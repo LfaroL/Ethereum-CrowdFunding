61edcaa8e2aef0d11c40c7bfdcf27d300cea84de
LfaroL <lfarol0228@gmail.com>
2019-09-23 10:29:40 -0700

    Added comments for clarity

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -5,8 +5,10 @@ contract CrowdFunding {
 
 	using Utils for *;
 
+	// Campaign states
 	enum State { Ongoing, Failed, Succeeded, Paidout }
 
+	// Event to emit for when campaign is finished
 	event CampaignFinished(
 		address addr,
 		uint totalCollected,
@@ -22,6 +24,7 @@ contract CrowdFunding {
 	bool public targetReached;
 	uint public totalCollected;
 
+	// Modifier that requires a function to be in a specific state
 	modifier inState(State expectedState) {
 		require(state == expectedState, "Invalid state");
 		_;
@@ -42,17 +45,24 @@ contract CrowdFunding {
 		state = State.Ongoing;
 	}
 
+	// inState requires the campaign to be ongoing to contribute
 	function contribute() public payable inState(State.Ongoing) {
+		// requires the date to be before the deadline
 		require(beforeDeadline(), "No contributions after the deadline");
+		// Using the mapping to indicate how much each account has added
 		amounts[msg.sender] += msg.value;
+		// Add to total collected amount
 		totalCollected += msg.value;
 
+		// Specify that the target amount is reached
 		if (totalCollected >= targetAmount) {
 			targetReached = true;
 		}
 	}
 
+	// inState requires the campaign to succeed for beneficiary to collect
 	function collect() public inState(State.Succeeded) {
+		// Use send method to send ether, if fails set state to Failed
 		if (beneficiary.send(totalCollected)) {
 			state = State.Paidout;
 		} else {
@@ -60,25 +70,31 @@ contract CrowdFunding {
 		}
 	}
 
+	// inState requires the campaign to have failed for accounts to withdraw
 	function withdraw() public inState(State.Failed) {
+		// requires the account to have contributed something
 		require(amounts[msg.sender] > 0, "Nothing was contributed");
 		uint contributed = amounts[msg.sender];
 		amounts[msg.sender] = 0;
 
+		// Use send method to send back ether, if fails then set amount to equal the original contributed value again
 		if (!msg.sender.send(contributed)) {
 			amounts[msg.sender] = contributed;
 		}
 	}
 
 	function finishCrowdFunding() public inState(State.Ongoing) {
+		// requires the campaign to be after the deadline to finish it
 		require(!beforeDeadline(), "Cannot finish campaign before a deadline");
 
+		// if target reached, set to succeed, else campaign has failed
 		if (!targetReached) {
 			state = State.Failed;
 		} else {
 			state = State.Succeeded;
 		}
 
+		// emit an event with the specified arguments
 		emit CampaignFinished(address(this), totalCollected, targetReached);
 	}
 
--- test/test_crowd_funding_test.js
+++ test/test_crowd_funding_test.js
@@ -1,19 +1,25 @@
 let CrowdFunding = artifacts.require('./TestCrowdFunding');
 
+// Specify the test name and get accounts in the network were testing on through function(accounts)
 contract('CrowdFunding', function(accounts) {
 
+	// contract variable for each test
 	let contract;
+	// specify first account in the Ganache test network to be contractCreator
 	let contractCreator = accounts[0];
+	// specify second account in the test network to be the beneficiary
 	let beneficiary = accounts[1];
 
 	const ONE_ETH_NUMBER = 1000000000000000000
-	const ONE_ETH = web3.utils.toBN(ONE_ETH_NUMBER);
+	const ONE_ETH = web3.utils.toBN(ONE_ETH_NUMBER); // convert to Big Number because that's the type of numbers used in an Ethereum network
 	const ERROR_MSG = "Returned error: VM Exception while processing transaction: revert";
+	// convert int to Big Number because that's the type that enum uses for each state
 	const ONGOING_STATE = web3.utils.toBN(0);
 	const FAILED_STATE = web3.utils.toBN(1);
 	const SUCCEEDED_STATE = web3.utils.toBN(2);
 	const PAID_OUT_STATE = web3.utils.toBN(3);
 
+	// before each test, setup the crowdfunding smart contract
 	beforeEach(async function() {
 		contract = await CrowdFunding.new(
 			'funding',
@@ -27,13 +33,17 @@ contract('CrowdFunding', function(accounts) {
 		);
 	});
 
+	// test to ensure each field is properly initialized
 	it('contract is initialized', async function() {
+		// contract.variable.call() to get the variable
 		let campaignName = await contract.name.call();
 		expect(campaignName).to.equal('funding');
 
+		// to.deep.equal() for Big Numbers
 		let targetAmount = await contract.targetAmount.call();
 		expect(targetAmount).to.deep.equal(ONE_ETH);
 
+		// toNumber() to convert Big Number
 		let fundingDeadline = await contract.fundingDeadline.call();
 		expect(fundingDeadline.toNumber()).to.equal(600);
 
@@ -44,11 +54,12 @@ contract('CrowdFunding', function(accounts) {
 		expect(state.valueOf()).to.deep.equal(ONGOING_STATE);
 	});
 
+	// test to ensure totalCollected is increased when contribute method is called
 	it('funds are contributed', async function () {
-		await contract.contribute({
-			value: ONE_ETH,
-			from: contractCreator
-		});
+		// when calling a method that requires a value of ether
+		// place the value and source address as a parameter in JSON format
+		// value: etherAmountInBN | from: addressOfSender
+		await contract.contribute({value: ONE_ETH,from: contractCreator});
 
 		// amounts is a mapping. put the parameter in call function. amounts[address]
 		let contributed = await contract.amounts.call(contractCreator);
@@ -65,6 +76,8 @@ contract('CrowdFunding', function(accounts) {
 				value: ONE_ETH_NUMBER,
 				from: contractCreator
 			});
+			// when expecting a test to fail, use this method so that 
+			// when it fails with the specified error, the test will pass
 			expect.fail();
 		} catch (error) {
 			expect(error.message).to.equal(ERROR_MSG);
@@ -93,11 +106,13 @@ contract('CrowdFunding', function(accounts) {
         await contract.setCurrentTime(601);
         await contract.finishCrowdFunding();
 
+        // use web3 to get balance based on address
         let initAmount = await web3.eth.getBalance(beneficiary);
-        await contract.collect({from: contractCreator});
+        await contract.collect();
 
         let newBalance = await web3.eth.getBalance(beneficiary);
         let difference = newBalance - initAmount;
+        // getBalance returns a number, not BN
         expect(difference).to.equal(ONE_ETH_NUMBER);
 
         let fundingState = await contract.state.call()
@@ -109,7 +124,7 @@ contract('CrowdFunding', function(accounts) {
         await contract.setCurrentTime(601);
         await contract.finishCrowdFunding();
 
-        await contract.withdraw({from: contractCreator});
+        await contract.withdraw();
         let amount = await contract.amounts.call(contractCreator);
         expect(amount).to.deep.equal(web3.utils.toBN(0));
     });
@@ -119,7 +134,9 @@ contract('CrowdFunding', function(accounts) {
     	await contract.setCurrentTime(601);
     	await contract.finishCrowdFunding();
 
+    	// get past events from the specified event
     	let events = await contract.getPastEvents(contract.CampaignFinished());
+    	// this is the emitted event based on the finishCrowdFunding() method
     	let event = events[0];
     	expect(event.args.totalCollected.toNumber()).to.equal(0);
     	expect(event.args.succeeded).to.equal(false);
